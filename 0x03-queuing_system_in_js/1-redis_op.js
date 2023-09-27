a2c92aa8d279e496ac57dd6705a557b36ba57e1f
whalay <soliuy5@gmail.com>
2023-09-27 17:42:04 +0100

    added

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,27 @@
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, (err, reply) => {
+    print(`Reply: ${reply}`);
+  });
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, reply) => {
+    console.log(reply);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,29 @@
+import { createClient, print } from 'redis';
+import { promisify } from 'util';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
+const getAsync = promisify(client.get).bind(client);
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, (err, reply) => {
+    print(`Reply: ${reply}`);
+  });
+}
+
+async function displaySchoolValue(schoolName) {
+  const reply = await getAsync(schoolName);
+  console.log(reply);
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
